ca35281fa404bceb4e02be9b5e2eebc66c418852
smx <123@qq.com>
2019-07-29 20:59:43 +0800

    codes commit

---
+++ StudyCodes/Python_就业/PythonAdvanced/Python提高/importmodule.py
@@ -0,0 +1,18 @@
+"""导入模块"""
+
+import sys
+
+# sys.path.append("/home/xyj") # 临时的
+
+path = sys.path
+
+print(path)
+
+""" 运行结果
+['/home/xyj/files/projects/mygits/StudyResources/StudyCodes/Python_就业/PythonAdvanced/Python提高', 
+'/home/xyj/.vscode/extensions/ms-python.python-2019.6.24221/pythonFiles', 
+'/home/xyj/.conda/envs/xyj1/lib/python37.zip', 
+'/home/xyj/.conda/envs/xyj1/lib/python3.7', 
+'/home/xyj/.conda/envs/xyj1/lib/python3.7/lib-dynload', 
+'/home/xyj/.conda/envs/xyj1/lib/python3.7/site-packages']
+"""
--- StudyCodes/Python_就业/PythonAdvanced/Python提高/othercopy.py
+++ StudyCodes/Python_就业/PythonAdvanced/Python提高/othercopy.py
@@ -1,5 +1,7 @@
 """其他拷贝的方式"""
 
+import copy
+
 # 分片表达式可以赋值一个序列
 
 # a = [11, 22]
@@ -29,10 +31,44 @@ id of d[0]: 2151015855176
 
 # 字典的copy方法可以拷贝一个字典
 
-d = dict(name = "zhangsan", age = 27)
+# d = dict(name = "zhangsan", age = 27, li = [1, 2])
 
-co = d.copy()
+# co = d.copy()
 
-print("id of c:", id(c))
+# d["li"].append(3)
 
-print("id of d:", id(d))
+# print("id of d:", id(d))
+
+# print("id of co:", id(co))
+
+# print("d is:", d)
+
+# print("co is:", co)
+
+"""
+id of c: 140195755895184
+id of d: 140195755894248
+d is: {'name': 'zhangsan', 'age': 27, 'li': [1, 2, 3]}
+co is: {'name': 'zhangsan', 'age': 27, 'li': [1, 2, 3]}
+"""
+
+a = [1, 2, 3]
+b = copy.copy(a)
+
+print("id of a is:", id(a))
+print("id of b is:", id(b))
+
+c = (1, 2, 3)
+d = copy.copy(c)
+
+print("id of c is:", id(c))
+print("id of d is:", id(d))
+
+"""运行结果
+id of a is: 140491659547336
+id of b is: 140491659548936
+id of c is: 140491659501216
+id of d is: 140491659501216
+"""
+
+
