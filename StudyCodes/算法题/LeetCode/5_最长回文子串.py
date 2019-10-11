0e137cc27e9449088e4a02ea18f89bfbe26dd187
smxJ0 <1234@qq.com>
2019-10-11 18:36:34 +0800

    codes commit

--- StudyCodes/算法题/LeetCode/5_最长回文子串.py
+++ StudyCodes/算法题/LeetCode/5_最长回文子串.py
@@ -21,5 +21,37 @@ class Solution:
                 maxCount = len(temps)
                 huiwenStr = temps
         return huiwenStr
-            
-        
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        size = len(s)
+        max_len = 0
+        ret = ""
+        for left in range(size):
+            tempS = s[left::]
+            for right in range(len(tempS)):
+                tempS2 = tempS[:right+1]
+                if tempS2 == tempS2[::-1]:
+                    if len(tempS2) > max_len:
+                        max_len =len(tempS2)
+                        ret = tempS2
+        return ret
+
+# 方法二：
+class Solution:
+    def longestPalindrome(self, s1: str) -> str:
+        size = len(s1)
+        dp = [[False for t in range(size)] for _ in range(size)]
+        if size <= 1:
+            return s1
+        max_len = 0
+        ret = s1[0]
+        for right in range(1, size):
+            for left in range(right):
+                if s1[left] == s1[right] and (right - left <= 2 or dp[left + 1][right - 1]):
+                    dp[left][right] = True
+                    current_len = right - left + 1
+                    if current_len >= max_len:
+                        max_len = current_len
+                        ret = s1[left:right+1]
+        return ret
