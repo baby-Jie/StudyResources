8777217b01c6df21da6f17ea2d58bcfca0e23c27
smxJ0 <1234@qq.com>
2019-10-22 23:10:46 +0800

    codes commit

--- TestCodes/test.py
+++ TestCodes/test.py
@@ -1,31 +1,13 @@
-
-import itertools as it
-
-N = int(input().strip())
-li = input().strip().split(" ")
-li = [int(i) for i in li]
-
-li2 = list(range(1, N + 1))
-test0 = it.permutations(li2, len(li2))
-test0 = list(test0)
-count = 0
-fuhe_li = list(range(len(test0)))
-bufuhe_li = []
-length = len(li)
-
-for i in range(length):
-    for index in fuhe_li:
-        if index in bufuhe_li:
-            continue
-        if li[i] == 0:
-            if test0[index][i] > test0[index][i + 1]:
-                bufuhe_li.append(index)
-        else:
-            if test0[index][i] < test0[index][i + 1]:
-                bufuhe_li.append(index)
-
-for index in fuhe_li:
-    if index not in bufuhe_li:
-        count += 1
-
-print(count % (10**9 + 7))
+try:
+    number=int(input())
+    expr=''
+    for i in range(0,number):
+        n=input()
+        expr=expr+n+'\n'
+        if i ==number-1:
+            index=n.index('=')
+            output=n[0:index]
+            expr=expr+'print('+output+')'
+    exec(expr)
+except Exception as e:
+    print('NA')
