eb8320ba230ad349162ae148adbea9d86fdccc1b
xingge <995047027@qq.com>
2020-01-06 07:36:23 +0800

    1

--- Coding/ComputerBasic/LInux/Shells/FoldStructor.sh
+++ Coding/ComputerBasic/LInux/Shells/FoldStructor.sh
@@ -1,5 +1,10 @@
 #!/bin/usr/bash
 fileName=file.txt
+if [ $# -eq 0 ];then
+    echo "length is 0"
+    exit
+fi
+
 if [ !-f $fileName ]; then
     echo "$fileName is not a file"
     exit
@@ -7,5 +12,6 @@ fi
 
 for directoryPath in `cat $fileName`
 do 
+    if [ $]
     mkdir -p $directoryPath
 done
